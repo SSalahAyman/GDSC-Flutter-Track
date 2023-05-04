1179cbd476917808c5e4c190ccfa36a0b99fef94
Salah Ayman <59060196+SSalahAyman@users.noreply.github.com>
2023-05-04 17:16:29 +0300

    task 8 of Dart is done

---
+++ Dart Tasks/task 8/bin/task_8.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+
+void main(){
+  for(int i=901;i<=926;i++){
+    print( "$i 💍🤍");
+  }
+}
---
+++ Dart Tasks/task 8/test/task_8_test.dart
@@ -0,0 +1,8 @@
+import 'package:task_8/task_8.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
