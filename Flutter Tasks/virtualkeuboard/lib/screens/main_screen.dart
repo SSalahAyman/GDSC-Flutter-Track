e10b1a71d15f2b84bbcafa73a32d8b88341bfadc
Salah Ayman <59060196+SSalahAyman@users.noreply.github.com>
2023-07-20 01:35:22 +0300

    strucure of main page is preapred

--- Flutter Tasks/virtualkeuboard/lib/screens/main_screen.dart
+++ Flutter Tasks/virtualkeuboard/lib/screens/main_screen.dart
@@ -2,9 +2,38 @@ import 'package:flutter/material.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
-
+  static late double width;
+  static late double height;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    width = MediaQuery.of(context).size.width;
+    height = MediaQuery.of(context).size.height;
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0xff25292f),
+        appBar: AppBar(
+          title: Text("Custom Keyboard"),
+        ),
+        body: Column(
+          children: [
+            Container(
+              color: Color(0xff25292f),
+              width: width,
+              height: height * 0.5,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(25),
+                    topRight: Radius.circular(25)),
+                color: Color(0xff2d333d),
+              ),
+              width: width,
+              height: height * 0.4051,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
